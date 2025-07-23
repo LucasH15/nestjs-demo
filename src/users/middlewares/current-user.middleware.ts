592b69ea34c0246210f6fc7b5d82e655bac5e916
Lucas Hubert <hello@lucas-hubert.me>
2025-07-23 07:05:54 +0200

    Add security

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private usersService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req.session || {};
+
+    if (userId) {
+      // @ts-expect-error currentUser
+      req.currentUser = await this.usersService.findOne(userId);
+    }
+
+    next();
+  }
+}
